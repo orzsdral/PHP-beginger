30483cfbd354c49ab83b8adc6afbb7152abd0c39
chenglung <orzsdral@gmail.com>
2024-12-25 00:11:28 +0800

    Variable

--- hello.php
+++ hello.php
@@ -1,3 +1,8 @@
 <?
+$name = "Anthony";
+$age = 35;
+$salary_point = 280;
+$salary_point_per = 16.5;
+$salary = $salary_point * $salary_point_per;
 
-echo "Hello, World";
+echo "My Name is $name, Age is $age, Salary is $salary";
