98dada139576897437f3e0d0e0546fe70f3e9b7c
chenglung <orzsdral@gmail.com>
2024-12-24 23:59:05 +0800

    Lesson1 echo Hello World!

--- hello.php
+++ hello.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello</h1>
+<?
 
-    <p>This git test</p>
-
-    <ol>
-        <li>someboy is cool</li>
-
-    </ol>
-</body>
-</html>
+echo "Hello, World";
