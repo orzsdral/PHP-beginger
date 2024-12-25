e4301158016fe418187cb1e0c0374ce917d15627
Anthony <astchengl@ccu.edu.tw>
2024-12-25 09:35:52 +0800

    建立資料庫和表跟資料

---
+++ SQL/cms.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost:3306
+-- 產生時間： 2024-12-25 01:23:24
+-- 伺服器版本： 5.7.24
+-- PHP 版本： 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `article`
+--
+
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
+  `title` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `published_at` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `article`
+--
+
+INSERT INTO `article` (`id`, `title`, `content`, `published_at`) VALUES
+(1, 'First Post', 'his is the first post', '2024-12-02'),
+(3, 'Second Post', 'This is the second post', '2024-12-09'),
+(4, 'Third Post', 'This is the third post', '2024-12-16'),
+(5, 'Fourth Post', 'This is the fourth post', '2024-12-23');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
