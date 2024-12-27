207d032ba414b679314e2d8608fc224e2246e8d1
Anthony <astchengl@ccu.edu.tw>
2024-12-27 14:43:15 +0800

    將使用者所填資料倒入資料庫

--- new-article.php
+++ new-article.php
@@ -1,8 +1,29 @@
 <?php
 
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        var_dump($_POST);
+        //引入db.php
+        require_once('includes/db.php');
+
+        //取得表單資料
+        $title = $_POST['title'];
+        $content = $_POST['content'];
+        $published_at = $_POST['published_at'];
+
+        //寫入資料庫
+        $sql = "INSERT INTO article(title, content, published_at)
+                VALUES('$title', '$content', '$published_at')";
+        $results = mysqli_query($conn, $sql);
+        //判斷$result===false,代表SQL語法有誤
+        if($results === false){
+            //顯示錯誤訊息
+            echo mysqli_error($conn);
+            exit;
+        }
+        //mysqli_insert_id()函數返回上一個查詢中自動生成的ID
+        $id = mysqli_insert_id($conn);
+        echo "Inserted article with id: $id";
     }
+      
 ?>
 <?php require_once('includes/header.php'); ?>
 
