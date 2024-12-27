e82ab5a8c747d35d033f99a8301cbf7444360e9f
Anthony <astchengl@ccu.edu.tw>
2024-12-27 10:28:05 +0800

    article加入判斷防止使用者輸入非數字值

--- article.php
+++ article.php
@@ -11,21 +11,28 @@
     if(!$conn){
         echo 'Connection error: ' . mysqli_connect_error();
     }           
-    //抓取資料庫的資料
-    $sql = 'SELECT * 
-            FROM article
-            WHERE id = ' . $_GET['id'];
+    
+    //加入判斷Get來的id是否有值且為數字,此作法可避免使用者透過網址列來輸入非數字的值
+    if(isset($_GET['id']) && is_numeric($_GET['id'])){
+        //抓取資料庫的資料
+        $sql = 'SELECT * 
+                FROM article
+                WHERE id = ' . $_GET['id'];
 
-    $results = mysqli_query($conn, $sql);
-    //判斷$result===false,代表SQL語法有誤
-    if($results === false){
-        //顯示錯誤訊息
-        echo mysqli_error($conn);
-        exit;
+        $results = mysqli_query($conn, $sql);
+        //判斷$result===false,代表SQL語法有誤
+        if($results === false){
+            //顯示錯誤訊息
+            echo mysqli_error($conn);
+            exit;
+        }
+        // $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+        //改由mysqli_fetch_assoc()來取得資料,從結果集中取得一行作為關聯數組，若無抓回東西會回傳NULL
+        $articles = mysqli_fetch_assoc($results);
+    }else{
+        //若沒有id值或id值不是數字則顯示null
+        $articles = null;
     }
-    // $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
-    //改由mysqli_fetch_assoc()來取得資料,從結果集中取得一行作為關聯數組，若無抓回東西會回傳NULL
-    $articles = mysqli_fetch_assoc($results);
 
 
 ?>
