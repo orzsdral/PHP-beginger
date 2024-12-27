dd5b8a9cffa439c103cda743bfa4aeaf8b84bc08
Anthony <astchengl@ccu.edu.tw>
2024-12-27 11:44:19 +0800

    認識輸入型態

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <?php
+//$_SERVER['REQUEST_METHOD']是一個超全域變數，包含了網頁請求的資訊 看是用GET還是POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $val = $_POST['val'];
@@ -8,7 +9,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo $val2;
 }
 
-    ?>
+?>
+
 <!DOCTYPE html>
 <html lang="zh-tw">
 <head>
@@ -16,10 +18,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Form</title>
 </head>
 <body>
+    <!-- 設定表單方式為post -->
     <form method="post">
         <input type="text" name="val">
-        <input type="text" name="val2">
-        <button>send</button>
+        <input type="email" name="mail">
+        <input type="submit" name="submit">
+        <input type="number" name="number">
+        <input type="password" name="password">
+        <input type="date" name="date">
+        <input type="time" name="time">
+        <input type="url" name="url">
+        <input type="tel" name="tel">
+        <input type="color" name="color">
+        <input type="range" name="range">
+        <input type="file" name="file">
+        <input type="hidden" name="hidden">
+        <input type="submit" name="submit">
+        <input type="reset" name="reset">
+        <input type="radio" name="radio">
+        <input type="checkbox" name="checkbox">
+        <input type="image" name="image">
+        <input type="button" name="button">
+        <input type="datetime-local" name="datetime-local">
+        <input type="month" name="month">
+        <input type="week" name="week">
+        <input type="other" name="other">
     </form>
 </body>
 </html>
