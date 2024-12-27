29c85e903afd8aa0f53722c0a05a9ef3b98629a4
Anthony <astchengl@ccu.edu.tw>
2024-12-27 11:34:35 +0800

    form初步認識

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php
+var_dump($_GET);
+var_dump($_GET['val']);
+
+    ?>
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head>
+    
+    <title>Form</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="val">
+        <input type="text" name="val2">
+        <button>send</button>
+    </form>
+</body>
+</html>
