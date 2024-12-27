0fd358c97ed13a6691d8cc37861e728ad12ffb09
Anthony <astchengl@ccu.edu.tw>
2024-12-27 09:00:18 +0800

    建立index面串接db加入判斷並果集為空顯示無文章發現

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <?php
-//先硬編模擬資料庫的資料，在此增加陣列內容及可變動顯示的文章
-$articles = [
-    [
-        'title'   => 'First Post', 
-        'content' => 'This is the first post'
-    ],
-    [
-        'title'   => 'Second Post', 
-        'content' => 'This is the second post'
-    ],
-    [   
-        'title'   => 'Third Post', 
-        'content' => 'This is the third post'
-    ],
-    [
-        'title'   => 'Fourth Post', 
-        'content' => 'This is the fourth post'
-    ]
-];
+    $db_host = 'localhost';
+    $db_user = 'Anthony';
+    $db_password = 'LIN';
+    $db_name = 'cms';
+
+    //建立與資料庫連線
+    $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+    //判斷$conn是否有物件回傳來，"有"就會值就為True;"沒有"就會回傳False，在用!來取反義
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }           
+    //抓取資料庫的資料
+    $sql = 'SELECT * 
+            FROM article';
+
+    $results = mysqli_query($conn, $sql);
+    //判斷$result===false,代表SQL語法有誤
+    if($results === false){
+        //顯示錯誤訊息
+        echo mysqli_error($conn);
+        exit;
+    }
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="zh-tw">
@@ -33,14 +40,19 @@ $articles = [
     </header>
     <!-- 讀取出陣列鍵和值 -->
     <main>
-        <ul>
+        <ol>
+        <!-- 加入判斷如果數組是空的不能顯示 -->
+        <?php if(empty($articles)): ?>
+            No articles found.
+        <?php else: ?>
             <?php foreach($articles as $article): ?>
                 <li>
                     <h2><?php echo $article['title']; ?></h2>
                     <p><?php echo $article['content']; ?></p>
                 </li>
             <?php endforeach; ?>
-        </ul>
+        <?php endif; ?>
+        </ol>
     </main>
 </body>
 </html>
