0383b8202083d87beecd450a4af623ba545f2f03
Anthony <astchengl@ccu.edu.tw>
2024-12-30 16:33:26 +0800

    加入登入介面和使用者認證處理

--- login.php
+++ login.php
@@ -1,5 +1,36 @@
 <?php
-
+require_once('includes/url.php');
 session_start();
 
-$_SESSION['is_logged_in'] = true;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_POST['username'] === 'Anthony' && $_POST['password'] === 'LIN') {
+        $_SESSION['is_logged_in'] = true;
+        
+        redirect("/PHP-beginger/index.php");
+        exit;
+    } else {
+        $error = '帳號或密碼錯誤';
+    }
+}
+
+
+require_once('includes/header.php');
+if (!empty($error)){
+    echo "<p>{$error}</p>";
+}
+echo <<<_END
+    <h2>Login</h2>
+    <form method="post" action="login.php">
+        <div>
+            <label for="username">帳號:</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="password">密碼:</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <input type="submit" value="Login">
+    </form>
+    _END;
+
+require_once('includes/footer.php');
--- logout.php
+++ logout.php
@@ -1,5 +1,17 @@
 <?php
-
+require_once('includes/url.php');
 session_start();
 
 $_SESSION['is_logged_in'] = false;
+
+require_once('includes/header.php');
+
+if (!empty($error)){
+    echo "<p>{$error}</p>";
+}
+echo <<<_END
+    <h2>Logout</h2>
+    <p>您已登出</p>
+    _END;
+redirect("/PHP-beginger/index.php");
+require_once('includes/footer.php');
