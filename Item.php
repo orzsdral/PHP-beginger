1f1c864c8cdb8133fa03ae4cd00d93ba95c99df6
Anthony <astchengl@ccu.edu.tw>
2024-12-31 14:19:03 +0800

    物件方法

--- Item.php
+++ Item.php
@@ -1,8 +1,15 @@
 <?php
-//定義類別名稱英大寫字母開頭
+//其內部通常會先定義屬性 方法在下方
 class Item{
     
     public $name;
     public $descript = 'the old post';
 
+    function sayHello(){
+        echo 'Hello';
+    }
+
+    function getName(){
+        return $this->name;   //特殊變量this,表示當前對象(實例)
+    }
 }
--- oo-ex.php
+++ oo-ex.php
@@ -4,8 +4,7 @@ require "Item.php";
 
 $my_item = new Item();
 
-$my_item->name = 'example';
-$my_item->descript = 'a new post';
-$my_item->price = 12.99;  //PHP允許不需先定義在類別物件裡面也可使用，但不建議這樣使用 不易維護
+$my_item->sayHello();
 
-var_dump($my_item->price);
+$my_item->name = 'Jame';
+echo ', '. $my_item->getName();
