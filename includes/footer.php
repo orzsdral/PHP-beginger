7398b7d9e8e6b1522bfb205793b0232cc0987c9a
Anthony <astchengl@ccu.edu.tw>
2025-03-05 16:46:49 +0800

    增加JQuery和一個Custom Script file 到CMS

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
 </main>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="/PHP-beginger/js/script.js"></script>
 </body>
 </html>
--- myscript.js
+++
@@ -1,7 +0,0 @@
-function sayHello(){
-    alert('hello3');
-}
-
-//document.getElementById('btn1').addEventListener('click',sayHello);
-
-$("#btn1").on('click',sayHello);
