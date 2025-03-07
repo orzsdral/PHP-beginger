5b24912b52db2757c07d0d2cc5cf1ba3ab7a9c2a
Anthony <astchengl@ccu.edu.tw>
2025-03-07 15:38:11 +0800

    加入bootstrap資源和添加常規布局並設置導航鏈接模式

--- includes/header.php
+++ includes/header.php
@@ -2,26 +2,28 @@
 <html lang="zh-tw">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>My Blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-   
+    <div class="container">
+
     <header>
         <h1>My Blog</h1>
     </header>
  
     <nav>
-        <ul>
-            <li><a href="/PHP-beginger/">首頁</a></li>
+        <ul class="nav">
+            <li class="nav-item"><a href="/PHP-beginger/">首頁</a></li>
             <?php if (Auth::isLoggedIn()) :?>
-                <li><a href="/PHP-beginger/admin/">管理者介面</a></li>
-                <li><a href="/PHP-beginger/logout.php">登出</a></li>
+                <li class="nav-item"><a href="/PHP-beginger/admin/">管理者介面</a></li>
+                <li class="nav-item"><a href="/PHP-beginger/logout.php">登出</a></li>
             <?php else:?>
-                <li><a href="/PHP-beginger/login.php">登入</a></li>
+                <li class="nav-item"><a href="/PHP-beginger/login.php">登入</a></li>
             <?php endif;?>
 
-            <li><a href="/PHP-beginger/contact.php">聯絡我們</a></li>
+            <li class="nav-item"><a href="/PHP-beginger/contact.php">聯絡我們</a></li>
         </ul>
 
     </nav>
--- includes/pagination.php
+++ includes/pagination.php
@@ -1,20 +1,20 @@
 <?php $base = strtok($_SERVER['REQUEST_URI'],'?'); ?>
 
 <nav>
-    <ul>
+    <ul class="pagination">
         <?php if($paginator->previous): ?>
-            <li>
-                <a <?= "href={$base}?page={$paginator->previous}"; ?>>上一頁</a>
+            <li class="page-item">
+                <a class="page-link" <?= "href={$base}?page={$paginator->previous}"; ?>>上一頁</a>
             </li>
         <?php else: ?>
-            <li>上一頁</li>
+            <li class="page-link">上一頁</li>
         <?php endif; ?>
         <?php if($paginator->next): ?>
-            <li>
-                <a <?= "href={$base}?page={$paginator->next}"; ?>>下一頁</a>
+            <li class="page-item">
+                <a class="page-link" <?= "href={$base}?page={$paginator->next}"; ?>>下一頁</a>
             </li>
         <?php else: ?>
-            <li>下一頁</li>
+            <li class="page-link">下一頁</li>
         <?php endif; ?>
         
     </ul>
